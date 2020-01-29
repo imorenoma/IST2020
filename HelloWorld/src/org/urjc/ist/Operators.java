0f8404bc707f77013a506e7dda58caf3861d772d
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-01-29 13:23:08 +0100

    Initial commit

---
+++ HelloWorld/src/org/urjc/ist/Operators.java
@@ -0,0 +1,16 @@
+package org.urjc.ist;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var1 = 0 , var2 = 0;
+		
+		var1++; // var1 vale 1
+		var2 --; //var2 ahora vale 0
+		var1 += var2; //equivale a: var1 = var1 + var2
+		
+		System.out.println("El numerin es: " + var1);
+	}
+
+}
