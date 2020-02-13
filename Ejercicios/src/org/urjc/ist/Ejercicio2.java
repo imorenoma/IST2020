dede72cc274dbfbe48b3e001db95bea74ed1aab9
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-02-13 18:32:14 +0100

    Exercice2 complete

--- Ejercicios/src/org/urjc/ist/Ejercicio2.java
+++ Ejercicios/src/org/urjc/ist/Ejercicio2.java
@@ -1,4 +1,4 @@
-package helloWorld;
+package org.urjc.ist;
 
 import java.util.Random;
 
@@ -9,7 +9,36 @@ public class Ejercicio2 {
 	
 	public static String longest(String a, String b) {
 		// Implementar el cuerpo del método
-		return "";
+		
+		if(a.length() > b.length()) {
+			return a;
+		}else if(a.length() < b.length()) {
+			return b;
+		}else {
+			return "a,b are equals";
+		}
+		
+	}
+	
+	public static void slices(String fullname) {
+		int i;
+		
+		String[] division = fullname.split(" ") ;
+		for(i = 0; i< division.length-1;i++) {
+			System.out.println(division[i]);
+		}
+	}
+	
+	public static void ran_num(double num, String name1, String name2, String name3) {
+		
+		if(num < 0.3) {
+			System.out.println(name1);
+		}else if(num > 0.3 && num <= 0.6) {
+			System.out.println(name2);
+		}else {
+			System.out.println(name3);
+		}
+		
 	}
 
 	public static void main(String[] args) {
@@ -22,7 +51,7 @@ public class Ejercicio2 {
 		// la cadena más larga de las tres definidas arriba
 		
 		System.out.println(longest(longest(cadena1, cadena2), cadena3));
-		
+		System.out.println("-------------------");		
 		/* 
 		 * Pregunta 2: Usando la interfaz de eclipse, explora qué otros
 		 * métodos hay disponibles para una variable de tipo String.
@@ -31,6 +60,7 @@ public class Ejercicio2 {
 		 * el nombre de los tres personajes 
 		 */
 		
+		slices(cadena1);
 		/*
 		 * Pregunta 3: El siguiente código muestra cómo usar un objeto de la
 		 * clase java.util.Random para generar secuencias de números pseudo-
@@ -38,8 +68,9 @@ public class Ejercicio2 {
 		 * que imprima cadena1 si el número obtenido es menor que 0.3, cadena2
 		 * si el número obtenido es menor que 0.6 y cadena3 en los restantes casos.
 		 */
-		
+		System.out.println("-------------------");
 		Random prng = new Random(); // Generador de números pseudo-aleatorios
-		System.out.println(prng.nextDouble());
+		System.out.println(prng.nextDouble());		;
+		ran_num(prng.nextDouble(), cadena1, cadena2, cadena3);
 	}
 }
