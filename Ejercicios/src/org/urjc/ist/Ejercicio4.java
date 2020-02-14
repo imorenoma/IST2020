8808340d98a1cc1231a9329a9879698021c5e99f
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-02-14 09:08:41 +0100

    Exercice4.java complete

--- Ejercicios/src/org/urjc/ist/Ejercicio4.java
+++ Ejercicios/src/org/urjc/ist/Ejercicio4.java
@@ -1,4 +1,6 @@
-package helloWorld;
+package org.urjc.ist;
+
+import java.util.Scanner;
 
 public class Ejercicio4 {
 	/*
@@ -7,4 +9,21 @@ public class Ejercicio4 {
 	 * debe imprimir por pantalla el nombre completo del mes que corresponde
 	 * al entero introducido.
 	 */
+	
+	public static void main(String[]args) {
+		
+		String[] calendar = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+				"Julio", "Agosto", "Septiembre","Octubre","Noviembre", "Diciembre"};
+				
+		Scanner input = new Scanner(System.in);
+		System.out.println("Insert a number");		
+		int number = input.nextInt();
+		int month = number - 1;
+		
+		if(number <1 || number > 12 ) {
+			System.out.println("Usage Error: argv[1] number between 1 & 12");
+		}else {
+			System.out.println("The Month is: "+ calendar[month]);
+		}
+	}
 }
