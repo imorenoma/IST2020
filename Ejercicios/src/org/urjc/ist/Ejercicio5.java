3cf937f8309069655f647d5907feb4b6861ebd89
Ivan Moreno <i.morenoma@alumnos.urjc.es>
2020-02-14 10:52:39 +0100

    Exercice7 Complete

--- Ejercicios/src/org/urjc/ist/Ejercicio5.java
+++ Ejercicios/src/org/urjc/ist/Ejercicio5.java
@@ -1,5 +1,6 @@
-package helloWorld;
+package org.urjc.ist;
 import java.util.Random;
+import java.util.Arrays;
 
 public class Ejercicio5 {
 	/*
@@ -12,5 +13,53 @@ public class Ejercicio5 {
 	 * Al concluir, el programa debe imprimir por pantalla el menor de todos los
 	 * números generados en esta segunda serie.
 	 */
+	
+	public static void fstRandom() {
+		
+		int[] even = new int[10];
+		int i = 0;
+		int count = 0;
+		
+		while(count <= 9) {
+			Random num = new Random();
+			int number = num.nextInt();
+			
+			if((number%2)==0) {
+				even[i]= number;
+				System.out.println(even[i] + " ");
+				i++;
+				count++;
+			}			
+		}		
+	}
+	
+	public static void scndRandom() {
+		
+		int[] maxnum = new int[20];
+		int counter2 =0;
+		int i = 0;
+		int j ;		
+		
+		while(counter2 <= 19) {
+			
+			Random numbers = new Random();
+			int num = numbers.nextInt();
+			
+			maxnum[i] = num;
+			i++;
+			counter2++;
+		}
+		
+		Arrays.sort(maxnum);
+		System.out.println(maxnum[0]);
+		
+		
+	}
+	public static void main(String[]args) {
+		
+		fstRandom();
+		System.out.println("-------------------");
+		scndRandom();
+	}
 	
 }
